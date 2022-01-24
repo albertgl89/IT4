7ff9a21ec3c4e6bc4ecd7e5d94feb732270dbb61
Albert Garcia Llorca <albertgarcia20@gmail.com>
2022-01-24 11:57:11 +0000

    - Styles mod to welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,27 +4,27 @@
 
 
 @section('content')
-    <div class="grid grid-flow-col gap-2 w-full mt-2">
+    <div class="grid grid-flow-row md:grid-flow-col gap-2 w-full mt-2">
 
-        <div class="rounded-lg grid grid-flow-row gap-2 shadow border-2 pb-2">
-            <div class="mb-2 w-full -m-1 mx-auto">
-                <p class="p-2 rounded-t-lg bg-indigo-900 text-white w-full">Gestió d'equips</p>
+        <div class="rounded-lg grid grid-flow-row gap-2 border border-indigo-900 shadow-md pb-2">
+            <div class="mb-2 w-full mx-auto">
+                <p class="p-2 rounded-t-md bg-indigo-900 border border-indigo-900 text-white w-full">Gestió d'equips</p>
             </div>
             <a href="{{url('teams')}}" class="green-pill-btn">Mostra equips</a>
             <a href="{{url('teams/add')}}" class="green-pill-btn">Crea equip</a>
         </div>
 
-        <div class="rounded-lg grid grid-flow-row shadow border-2 pb-2">
-            <div class="mb-2 w-full -m-1 mx-auto">
-                <p class="p-2 rounded-t-lg bg-indigo-900 text-white w-full">Gestió de partits</p>
+        <div class="rounded-lg grid grid-flow-row gap-2 border border-indigo-900 shadow-md pb-2">
+            <div class="mb-2 w-full mx-auto">
+                <p class="p-2 rounded-t-md bg-indigo-900 text-white w-full">Gestió de partits</p>
             </div>
             <a href="{{url('matches')}}" class="green-pill-btn">Mostra partits</a>
             <a href="{{url('matches/add')}}" class="green-pill-btn">Crea partit</a>
         </div>
 
-        <div class="rounded-lg grid grid-flow-row shadow border-2 pb-2">
-            <div class="mb-2 w-full -m-1 mx-auto">
-                <p class="p-2 rounded-t-lg bg-indigo-900 text-white w-full">Gestió de localitzacions</p>
+        <div class="rounded-lg grid grid-flow-row gap-2 border border-indigo-900 shadow-md pb-2">
+            <div class="mb-2 w-full mx-auto">
+                <p class="p-2 rounded-t-md bg-indigo-900 text-white w-full">Gestió de localitzacions</p>
             </div>
             <a href="{{url('locations')}}" class="green-pill-btn">Mostra localitzacions</a>
             <a href="{{url('locations/add')}}" class="green-pill-btn">Crea localització</a>
