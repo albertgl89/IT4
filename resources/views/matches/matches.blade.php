654fb519a64a032d90a85b68f7f601bec315158d
Albert Garcia Llorca <albertgarcia20@gmail.com>
2022-01-27 11:42:09 +0000

    - Teams and matches views styling

--- resources/views/matches/matches.blade.php
+++ resources/views/matches/matches.blade.php
@@ -9,41 +9,41 @@ use App\Models\MatchResult;
 
 
 @section('content')
-    <p>Llistat de tots els partits del sistema</p>
+    <p class="font-heebo">Tots els partits del sistema</p>
     <div class="grid grid-flow-col w-full mt-2">
 
         @if (session('status'))
-            <div class="rounded-xl bg-green-700 text-white font-bold p-2 w-3/4 mx-auto text-center">
+            <div class="rounded-xl bg-green-700 text-white font-bold p-2 w-3/4 mx-auto text-center font-heebo">
                 {{ session('status') }}
             </div>
         @endif
 
         @if (Match::all()->count() == 0)
             <div>
-                <p>No hi ha cap equip encara.</p>
+                <p class="font-heebo font-bold text-center">No hi ha cap equip encara.</p>
             </div>
         @endif
 
-        <div class="grid grid-flow-row md:grid-cols-4 gap-2">
+        <div class="grid grid-flow-row md:grid-cols-4 gap-2 font-heebo">
             @foreach (Match::all() as $match)
 
                 <div
-                    class="rounded-xl border border-indigo-900 hover:bg-indigo-600 bg-indigo-800 text-white min-w-fit max-w-1/4 p-2 m-2 grid grid-flow-col md:grid-flow-row gap-1 shadow-md md:items-center md:content-center justify-around">
+                    class="list-card-bg text-white min-w-fit max-w-1/4 p-2 m-2 grid grid-flow-col md:grid-flow-row gap-1 md:items-center md:content-center justify-around">
                    <div class="grid md:grid-flow-col gap-1 grid-flow-row h-min w-full place-self-center">
                     @if ($match->match_date > now())
-                    <div class="bg-orange-400 text-indigo-900 text-xs text-center w-full p-1 rounded-sm">
+                    <div class="bg-orange-400 text-indigo-900 text-xs font-bold text-center w-full p-1 rounded-sm">
                         <p>Pendent de disputar</p>
                     </div>
                 @else 
-                    <div class="bg-green-400 text-indigo-900 text-xs text-center w-full p-1 rounded-sm">
+                    <div class="bg-green-400 text-indigo-900 text-xs font-bold text-center w-full p-1 rounded-sm">
                         <p>Disputat</p>
                     </div>
                     @if ($match->match_result_id == null)
-                        <div class="bg-red-400 text-indigo-900 text-xs text-center w-full p-1 rounded-sm">
+                        <div class="bg-orange-400 text-indigo-900 text-xs font-bold text-center w-full p-1 rounded-sm">
                             <p>Esperant resultat</p>
                         </div>
                     @else 
-                        <div class="bg-green-400 text-indigo-900 text-xs text-center w-full p-1 rounded-sm">
+                        <div class="bg-green-400 text-indigo-900 text-xs font-bold text-center w-full p-1 rounded-sm">
                             <p>Resultat registrat</p>
                         </div>
                     @endif
@@ -58,12 +58,12 @@ use App\Models\MatchResult;
                         
                     </div>
                     
-                    <div class="grid grid-flow-col md:max-w-none max-w-[30%] justify-items-center content-center items-center md:gap-1 gap-2">
-                        <a href="matches/{{ $match->id }}" class="gold-pill-btn w-fit">Detalls</a>
-                        <a href="{{url('matches/'.$match->id.'/edit')}}"><span class="material-icons text-xl gold-pill-btn">
+                    <div class="grid grid-flow-col text-center md:flex md:flex-wrap md:max-w-none max-w-[30%] md:justify-items-around justify-items-center content-center items-center md:gap-2 gap-2">
+                        <a href="matches/{{ $match->id }}" class="gold-pill-btn w-fit md:w-full">Detalls</a>
+                        <a href="{{url('matches/'.$match->id.'/edit')}}" class="sm:inline hidden md:justify-self-stretch md:flex-1"><span class="material-icons text-xl gold-pill-btn md:w-full">
                             edit
                             </span></a>
-                            <a href="{{url('matches/'.$match->id.'/delete')}}"><span class="material-icons text-xl red-pill-btn">
+                        <a href="{{url('matches/'.$match->id.'/delete')}}" class="sm:inline hidden md:place-self-stretch md:flex-1"><span class="material-icons text-xl red-pill-btn md:w-full">
                                 delete
                                 </span></a>
                     </div>
--- resources/views/teams/teams.blade.php
+++ resources/views/teams/teams.blade.php
@@ -12,7 +12,7 @@ use App\Models\Team;
     <div class="grid grid-flow-col w-full mt-2 font-heebo">
 
         @if (session('status'))
-            <div class="rounded-xl bg-green-700 text-white font-bold p-2 w-3/4 mx-auto text-center">
+            <div class="rounded-xl bg-green-700 text-white font-bold p-2 w-3/4 mx-auto text-center font-heebo">
                 {{ session('status') }}
             </div>
         @endif
@@ -25,7 +25,7 @@ use App\Models\Team;
 
         <div class="grid grid-flow-row md:grid-cols-4 gap-2 font-heebo w-full">
             @foreach (Team::all() as $team)
-                <div class="md:flex md:flex-wrap rounded-tr-xl rounded-bl-xl shadow-lg hover:to-violet-900/80 bg-gradient-to-tl from-indigo-800  to-violet-900 text-white md:max-w-1/4 min-w-fit">
+                <div class="md:flex md:flex-wrap list-card-bg text-white md:max-w-1/4 min-w-fit">
                     <div
                         class="p-2 m-2 grid grid-cols-4 md:grid-cols-4 gap-1 md:items-center md:content-center justify-around">
                         
@@ -33,14 +33,14 @@ use App\Models\Team;
                             {{ $team->short_name }}</p>
                         <p class="col-start-2 col-end-3 md:col-span-2 md:col-start-3 place-self-center justify-self-start md:mx-auto">{{ $team->name }}</p>
                         
-                        <div class="m-2 p-2 mx-auto my-auto grid grid-flow-col md:flex md:flex-wrap justify-items-center content-center items-center gap-1 col-span-2 md:col-span-4 w-max md:w-full">
+                        <div class="p-2 mx-auto my-auto grid grid-flow-col md:flex md:flex-wrap justify-items-center content-center items-center gap-2 col-span-2 md:col-span-4 w-max md:w-full">
                             
                             <a href="teams/{{ $team->id }}" class="gold-pill-btn w-fit md:w-full text-center">Detalls</a>
-                            <a href="{{ url('teams/' . $team->id . '/edit') }}" class="sm:inline hidden w-1/3 mx-auto m-1"><span
+                            <a href="{{ url('teams/' . $team->id . '/edit') }}" class="sm:inline hidden w-1/3 md:flex-1 md:justify-self-stretch"><span
                                     class="material-icons text-xl gold-pill-btn md:w-full text-center">
                                     edit
                                 </span></a>
-                            <a href="{{ url('teams/' . $team->id . '/delete') }}" class="sm:inline hidden w-1/3 mx-auto m-1"><span
+                            <a href="{{ url('teams/' . $team->id . '/delete') }}" class="sm:inline hidden w-1/3 md:flex-1 md:justify-self-stretch"><span
                                     class="material-icons text-xl red-pill-btn md:w-full text-center">
                                     delete
                                 </span></a>
