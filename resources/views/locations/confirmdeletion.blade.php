3c08ddde860a53d8956106ac5e229cbb2a07aa93
Albert Garcia Llorca <albertgarcia20@gmail.com>
2022-01-30 16:29:23 +0000

    - Edit and delete location styled

--- resources/views/locations/confirmdeletion.blade.php
+++ resources/views/locations/confirmdeletion.blade.php
@@ -8,12 +8,12 @@ use App\Models\Location;
 
 
 @section('content')
-    <div class="grid grid-flow-row w-full mt-2">
+    <div class="grid grid-flow-row w-full mt-2 font-heebo">
 
-        <div class="w-3/4 mx-auto rounded-lg grid grid-flow-row shadow border-2 pb-2">
+        <div class="w-3/4 mx-auto rounded-br-xl rounded-tl-xl bg-white grid grid-flow-row shadow pb-2">
 
             <div class="mb-2 w-full -m-1 mx-auto">
-                <p class="p-2 rounded-t-lg bg-indigo-900 text-white w-full">
+                <p class="p-2 rounded-tl-xl bg-indigo-900 text-white w-full">
                     <a href="{{url('locations')}}" class="gold-pill-btn m-2 align-middle"><span class="material-icons text-xl align-middle">
                         arrow_back
                         </span>Torna</a>
@@ -33,13 +33,17 @@ use App\Models\Location;
                         <p>Els partits celebrats en aquesta ubicació <b>no es veuran afectats.</b></p>
                 </div>
                 
-                <p>Estadi: {{$location->stadium_name}}</p>
-                <p>Ciutat: {{$location->city}}, {{$location->state}}</p>
+                <p class="text-xl text-green-900 font-rubik"><span class="material-icons text-xl align-middle p-1">
+                    place
+                    </span>{{$location->stadium_name}}</p>
+                <p>{{$location->city}}, {{$location->state}}</p>
                 <p>Equips amb base en aquest estadi: </p>
                 <ul>
 
                     @forelse ($location->teams()->get() as $team)
-                        <li><a href="{{url('teams', [$team->id])}}" class="std-link">{{$team->name}}</a></li> 
+                        <li class="mt-2 mb-4"><a href="{{url('teams', [$team->id])}}" class="green-pill-btn w-fit text-center"><span class="material-icons text-xl align-middle p-1">
+                            group
+                            </span>{{$team->name}}</a></li> 
                     @empty
                         No hi ha cap equip amb base en aquest estadi encara.
                     @endforelse
@@ -50,14 +54,14 @@ use App\Models\Location;
                     @if (Location::where('city', $location->city)->count() <= 1)
                         No hi ha cap altre estadi en aquesta mateixa ciutat.
                     @else
-                        @foreach (Location::where('city', $location->city)->get() as $loc)
+                        @foreach (Location::where('city', $location->city)->where('id','!=', $location->id)->get() as $loc)
                             
-                            <li><a href="{{url('locations', [$loc->id])}}" class="std-link">{{$loc->stadium_name}}</a></li> 
+                            <li class="mt-2 mb-4"><a href="{{url('locations', [$loc->id])}}" class="green-pill-btn w-fit text-center"><span class="material-icons text-xl align-middle p-1">place</span>{{$loc->stadium_name}}</a></li> 
                                                 
                         @endforeach
                     @endif
                 </ul>
-                <p>Total de partits disputats en aquest estadi: TODO</p>
+                <p class="list-item-bg md:grid-cols-none grid-rows-none text-center w-3/4 mt-2 mb-2">Total de partits disputats en aquest estadi: {{$location->matches()->get()->count()}}</p>
             </div>
 
             <form action="{{url('locations/'.$location->id.'/delete')}}" method="post" class="std-form">
