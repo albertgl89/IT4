e11b58a4ceabdcdfef740ee8bfcb38bec81c9845
Albert Garcia Llorca <albertgarcia20@gmail.com>
2022-02-02 11:24:26 +0000

    - Base template upper menu improvements

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <!--Tailwind-->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <!--Material Icons-->
@@ -16,44 +17,51 @@
     <header class="w-full max-w-5xl mx-auto mt-2">
         <div
             class="rounded-tr-lg rounded-tl-lg bg-green-900 text-white text-3xl p-2 font-rubik flex flex-nowrap justify-between">
-            <p><span class="material-icons text-5xl p-2 align-top">
-                    sports_soccer
-                </span>MatchMaker</p>
-            <div>
+            <div class="flex flex-nowrap">
+                <div class="hidden md:inline my-auto m-1">
+                    <span class=" material-icons text-lg md:text-5xl">
+                        sports_soccer
+                    </span>
+                </div>
+                <p class="my-auto mr-2 text-lg md:text-3xl">MatchMaker</p>
+            </div>
+
+            <div class="grid grid-flow-col gap-1">
                 <a href="{{ url('/') }}"
                     class="md:inline hidden text-base p-2 align-middle gold-pill-btn h-fit w-fit"><span
-                        class="material-icons text-sm align-middle p-1">
+                        class="material-icons text-sm align-middle">
                         dashboard
                     </span>Dashboard</a>
                 <a href="{{ url('/') }}" class="inline md:hidden text-base p-2 align-middle gold-pill-btn"><span
-                        class="material-icons text-sm align-middle p-1 text-center">
+                        class="material-icons text-sm align-middle text-center">
                         dashboard
                     </span></a>
                 <a href="{{ url('teams') }}"
                     class="md:inline hidden text-base p-2 align-middle gold-pill-btn h-fit w-fit"><span
-                        class="material-icons text-sm align-middle p-1">
+                        class="material-icons text-sm align-middle">
                         group
                     </span>Equips</a>
                 <a href="{{ url('teams') }}" class="inline md:hidden text-base p-2 align-middle gold-pill-btn"><span
-                        class="material-icons text-sm align-middle p-1 text-center">
+                        class="material-icons text-sm align-middle text-center">
                         group
                     </span></a>
                 <a href="{{ url('matches') }}"
                     class="md:inline hidden text-base p-2 align-middle gold-pill-btn h-fit w-fit"><span
-                        class="material-icons text-sm align-middle p-1">
+                        class="material-icons text-sm align-middle">
                         sports_soccer
                     </span>Partits</a>
                 <a href="{{ url('matches') }}" class="inline md:hidden text-base p-2 align-middle gold-pill-btn"><span
-                        class="material-icons text-sm align-middle p-1 text-center">
+                        class="material-icons text-sm align-middle text-center">
                         sports_soccer
                     </span></a>
                 <a href="{{ url('locations') }}"
                     class="md:inline hidden text-base p-2 align-middle gold-pill-btn h-fit w-fit"><span
-                        class="material-icons text-sm align-middle p-1">
+                        class="material-icons text-sm align-middle">
                         place
                     </span>Localitzacions</a>
-                <a href="{{ url('locations') }}" class="inline md:hidden text-base p-2 align-middle gold-pill-btn"><span
-                        class="material-icons text-sm align-middle p-1 text-center">
+                <a href="{{ url('locations') }}"
+                    class="inline md:hidden text-base p-2 align-middle gold-pill-btn"><span
+                        class="material-icons text-sm align-middle text-center">
                         place
                     </span></a>
             </div>
